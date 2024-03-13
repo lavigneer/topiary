6c1b00ae290990337b0ed3f5c245916c90462f36
Eric Lavigne <hi_eric@hotmail.com>
2024-03-12 21:20:58 -0400

    fixes

--- topiary-queries/queries/css.scm
+++ topiary-queries/queries/css.scm
@@ -7,15 +7,18 @@
 ; Append space after colons
 ":" @append_space
 
+; Spacing before and after a rule_set
 (rule_set) @allow_blank_line_before
 (rule_set) @prepend_hardline
 
 ; Allow blank lines before any declaration in a block except the first one
 (block . (declaration) (declaration) @allow_blank_line_before)
 
+; Space before curly and after selectors
 [(selectors)] @append_space
 
 
+; Indent the declarations in the block
 (block
   .
   "{" @append_hardline @append_indent_start
@@ -37,3 +40,10 @@
   ";" @append_hardline
 )
 
+(selectors
+  "," @append_hardline
+)
+
+(block
+  "{" @append_hardline
+)
